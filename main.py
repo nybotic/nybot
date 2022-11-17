5545ccf9ec65b6208f26339082e342007827e30e
nybotic <80500434+nybotic@users.noreply.github.com>
2022-11-17 14:46:17 +1300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import discord
+
+TOKEN = ""
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("{0.user} is now online!".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello there!')
+
+client.run(TOKEN)
